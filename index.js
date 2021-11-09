652c5a8e7613872bc57589a7213a6e65c875b724
Robert Gaspar <gaspar.robert7@gmail.com>
2021-11-08 20:46:06 -0600

    commit tree f47bac047f97a4fe6d53a323fca9bf6a694bf86a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+// function superbowlWin(record) {
+//     let result = record.find((record) => record.result === "W")
+//     return superbowlWin(result) 
+// }
+
+let superbowlWin = (gameResult) => {
+    const result = gameResult.find(gameResult => gameResult.result === "W")
+    return !!result ? result.year : undefined
+}
